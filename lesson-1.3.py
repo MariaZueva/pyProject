f60d814623eab812311f65046cb982e3edacb062
ZuevaMary <zuevamary94@gmail.com>
2020-12-14 21:24:20 +0300

    lesson-1.3

--- lesson-1.3.py
+++ lesson-1.3.py
@@ -1,4 +1,2 @@
 n = input('Введите целое положительное число: ')
-nn = n + n
-nnn = nn + n
-print(f"{n} + {nn} + {nnn} = {int(n) + int(nn) + int(nnn)}")
+print(f"{n} + {n + n} + {n + n + n} = {int(n) + int(n + n) + int(n + n + n)}")
